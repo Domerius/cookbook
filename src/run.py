7aa300d1bf9cd87fa6453d85fda1364d55b2eca4
Domerius <155972096+Domerius@users.noreply.github.com>
2024-01-18 17:08:22 +0100

    [0.09] Cleaning after the experiment - Removed example class 'Person' from src/run.py - Removed tests from 'tests' directory for the erased class - Removed 'author' and 'readme' property from pyproject.toml (is to be added later)

--- src/run.py
+++ src/run.py
@@ -1,12 +1,3 @@
-class Person:
-    def __init__(self, name, age):
-        self.forname = name.split()[0]
-        self.surname = name.split()[-1]
-        self.age = age
-
-    def celebrate_birthsday(self):
-        self.age += 1
-
 def run():
     pass
 
