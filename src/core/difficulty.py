6e1e737d7f57fb5b49e24533c33a5f8380d73364
Domerius <155972096+Domerius@users.noreply.github.com>
2024-01-23 11:57:45 +0100

    [0.14] Created tests for the Difficulty class - Added tests for the Difficulty class - Modified the Difficulty class in order to compare its instances between each other

--- src/core/difficulty.py
+++ src/core/difficulty.py
@@ -7,3 +7,24 @@ class Difficulty(Enum):
     
     def __str__(self):
         return self.name.lower()
+    
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        else:
+            raise TypeError(f"The Difficulty class instances may be compared only between each other. " \
+                            f"Encountered types: {self.__class__} and {other.__class__}.")
+    
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        else:
+            raise TypeError(f"The Difficulty class instances may be compared only between each other. " \
+                            f"Encountered types: {self.__class__} and {other.__class__}.")
+    
+    def __eq__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value == other.value
+        else:
+            raise TypeError(f"The Difficulty class instances may be compared only between each other. " \
+                            f"Encountered types: {self.__class__} and {other.__class__}.")
