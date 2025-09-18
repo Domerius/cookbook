5427571554cae121769d50c533cecd257acd2442
Dominik <155972096+Domerius@users.noreply.github.com>
2025-09-18 08:34:41 +0200

    Revert "Core feature"

--- src/core/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('../helpers')
-
-from .difficulty import Difficulty
-from .ingredient import Ingredient
-from .recipe import Recipe
-from .cookbook import Cookbook
--- src/core/cookbook/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append('.../helpers')
-
-from .cookbook import Cookbook
